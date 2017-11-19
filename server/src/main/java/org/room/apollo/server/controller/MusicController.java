541cfb1d6927f86f30763f7a5d6086a1e4c87a96
Arsenii Zhdanov <arsenzhd@gmail.com>
2017-11-19 15:40:43 +0200

    Add swagger integration with spring

--- server/src/main/java/org/room/apollo/server/controller/MusicController.java
+++ server/src/main/java/org/room/apollo/server/controller/MusicController.java
@@ -7,6 +7,8 @@ import org.room.apollo.server.service.MusicService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
